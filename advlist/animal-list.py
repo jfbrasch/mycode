f62ef778cd28580fa8e36f611368cea006e6f785
jfbrasch <jfbrasch@yahoo.com>
2024-11-19 17:14:06 +0000

    mixed lists

--- advlist/animal-list.py
+++ advlist/animal-list.py
@@ -8,7 +8,7 @@ def main():
     animals3 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
 
     # display animals3
-    print(animals3)
+    print(animals3[0] + " " + animals3[1] + " " + animals3[2] + " " + animals3[3] + " " + animals3[4] + " " + animals3[5] + " " + animals3[6] + " " + animals3[7] + " " + animals3[8] + " " + animals3[9] + " " + animals3[10] + " " + animals3[11] + " " + animals3[12] + " " + animals3[13])
    
 
 main()
